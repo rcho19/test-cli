822758ed9c3d11932f5cbfa4590acb8dd37b62c2
rcho19 <ruichenghou@gmail.com>
2025-03-31 13:08:18 +0800

    doc: 更新readme.ms

---
+++ README.md
@@ -0,0 +1,29 @@
+# rcho-cli
+![Npm 版本](https://img.shields.io/badge/rcho-cli_v0.0.1-orange)
+
+## 简介
+- 用于快速搭建前端项目的命令行工具
+
+## 模板
+- 快速生成Vue3 + Vite4 + TS + ElementPuls + Pinia的模板
+
+
+## 安装
+
+```bash
+npm install rcho-cli -g
+```
+
+## 使用
+
+```bash
+# 使用模板创建新项目
+rcho create
+
+# 执行项目文件夹名称并选择模板创建新项目
+rcho create [projectName]
+
+# 查看脚手架版本
+rcho -v
+rcho --version
+```
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+/*
+ * @Desc: 
+ * @Author: Hrc
+ * @Date: 2025-03-17 12:06:46
+ * @LastEditors: Hrc
+ */
 import { Command } from 'commander';
 import { version } from '../package.json'
 import { create } from './command/create'
 
-const program = new Command('my-cli');
+const program = new Command('rcho');
 
 // 创建项目命令
 program.command('create').description('创建一个新项目').argument('[name]', '项目名称').action(async (dirName) => {
